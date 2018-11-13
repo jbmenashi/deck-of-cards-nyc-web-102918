a34779a4976fabf71a6a1c5ca051f0f7f80de3f8
jbmenashi <jbmenashi@gmail.com>
2018-11-13 10:11:41 -0500

    passed tests

--- solution.rb
+++ solution.rb
@@ -1,7 +1,38 @@
 class Deck
+  attr_accessor :cards
 
+  def initialize
+    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    cards_raw = suits.product(ranks)
+
+    @cards = cards_raw.map do |card|
+      Card.new(card[0], card[1])
+    end
+  end
+
+  def choose_card
+    drawn_card = self.cards.delete(self.cards.sample)
+  end
 end
 
 class Card
+  attr_accessor :suit, :rank
 
+  @@all  =[]
+
+  def initialize(suit, rank)
+    @rank = rank
+    @suit = suit
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def name
+    self.rank + "-" + self.suit
+  end
 end
